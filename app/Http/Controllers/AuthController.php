697a7fbb344142b618de292606f8feabdc4a382a
GabrielR4SH <bieelsantana23@hotmail.com>
2025-01-31 19:50:56 -0300

    feat: register method in AuthController & Token User

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $data = $request->validate([
+            'name' => ['required','string'],
+            'email' => ['required','email','unique:users'],
+            'password' => ['required','min:6']
+        ]);
+        
+        $user = User::create($data);
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return [
+            'user' => $user,
+            'token' => $token
+        ];
+    }
+
+}
